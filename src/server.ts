fc4fb9ad67285a8766ac478122266230df443a2f
williamwjd <william.jose.wjd@gmail.com>
2020-10-14 17:22:37 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import express from 'express';
 import { createConnection } from 'typeorm';
 import { resolve } from 'path';
+import cors from 'cors';
 
 import routes from './routes';
 import errorHandler from './errors/handler';
@@ -12,6 +13,7 @@ createConnection();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 app.use('/uploads', express.static(resolve(__dirname, '..', 'uploads')));
 app.use(errorHandler);
